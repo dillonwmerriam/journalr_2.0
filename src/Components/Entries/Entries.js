f10fb1b7cf97b81536a9c6a1c4d9bd8eb0516a09
Dillon Merriam <merriamdillon@yahoo.com>
2024-01-03 22:23:59 -0800

    Styled Home Screen

--- src/Components/Entries/Entries.js
+++ src/Components/Entries/Entries.js
@@ -1,6 +1,7 @@
 import './Entries.scss';
 
 export default function Entries() {
+    var date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"}) 
     var entries = [
     {entry: "test 1", date: "1/1/24"}, 
     {entry: "TEST 2", date: "1/1/24"}, 
@@ -8,13 +9,18 @@ export default function Entries() {
     ]
 
     return(
+      <div className='container'>
+        <div className='date'>
+          <span>{date}</span>
+        </div>
         <div className='entries'>
             {entries.map(item => (
-            <div className='entries'>
+            <div className='entry'>
                 <div dangerouslySetInnerHTML={{__html:item.entry}}></div>
             </div>
             )
             )}
         </div>
+      </div>
     )
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,14 +3,10 @@ import Entries from '../Components/Entries/Entries';
 import './Home.scss';
 
 export default function Home() {
-  var date = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"}) 
      return (
       <div className="Home">
         <div className="sections">
           <Header />
-          <div className="date">
-            <span>{date}</span>
-          </div>
           <Entries />
         </div>
       </div>
